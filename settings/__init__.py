b5b8ca6d86ca52215a0fcc51ea0ab726a4eefda9
Ali Rahimi <ali.rahimi.officalbiz@gmail.com>
2026-09-04 22:53:03 +0330

    Feat: Added SettingsPersistenceError to the __init__ file

--- settings/__init__.py
+++ settings/__init__.py
@@ -1,4 +1,4 @@
-from .manager import settings_manager
+from .manager import settings_manager, SettingsPersistenceError
 from .defaults import DEFAULT_SETTINGS
 
-__all__ = ["settings_manager", "DEFAULT_SETTINGS"]
+__all__ = ["settings_manager", "DEFAULT_SETTINGS", "SettingsPersistenceError"]
